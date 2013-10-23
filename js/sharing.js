267bb7251167a0c7e8ff731fc0d63c27423abd74
Stefano Vercelli <stefano.vercelli@telecomitalia.it>
2013-10-23 16:23:07 +0200

    Invoke dashboard for connection to mother's pzh.

--- js/sharing.js
+++ js/sharing.js
@@ -2,6 +2,18 @@
 function connectToBaby(babyName, motherName) {
     //TODO Connect to the pzh of mother and ask service sharing of baby db
     // Likely this func is async and can take long time to end - how to handle this???
-    alert('Connect to baby '+babyName+' and mom '+motherName);
+    //alert('Connect to baby '+babyName+' and mom '+motherName);
+
+    webinos.dashboard
+        .open({
+                module: 'people'
+            }, function(){
+                //alert('done');
+                }
+        )
+        .onAction(function (data) {
+            alert('action done: '+JSON.stringify(data));
+        });
+
 }
 
